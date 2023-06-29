aee2458935fe386633288f7ea61d3785dbd99aa1
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-06-29 13:56:03 +0400

    Footer finish

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,18 +1,33 @@
 import { Github, Linkedin } from "lucide-react";
 import React from "react";
 import styles from "./footer.module.scss";
+import Link from "next/link";
 export default function Footer() {
-  return (
-    <div className={styles.FooterWrapper}>
-      <div className={styles.Footer}>
-        <div className={styles.Footer__TextWrapper}>
-          <h1 className={styles.Footer__Text}>Powered by Beka Chaduneli</h1>
+    return (
+        <div className={styles.FooterWrapper}>
+            <div className={styles.Footer}>
+                <div className={styles.Footer__TextWrapper}>
+                    <h1 className={styles.Footer__Text}>
+                        Powered by Beka Chaduneli
+                    </h1>
+                </div>
+                <div className={styles.Footer__Socials}>
+                    <Link
+                        target="_blank"
+                        className={styles.Footer__IconLink}
+                        href="https://github.com/bekaChaduneli"
+                    >
+                        <Github className={styles.Footer__Icon} />
+                    </Link>
+                    <Link
+                        target="_blank"
+                        className={styles.Footer__IconLink}
+                        href="https://www.linkedin.com/in/beka-chaduneli-28203422b/"
+                    >
+                        <Linkedin className={styles.Footer__Icon}  />
+                    </Link>
+                </div>
+            </div>
         </div>
-        <div className={styles.Footer__Socials}>
-          <Github className={styles.Footer__Icon} />
-          <Linkedin className={styles.Footer__Icon} />
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
