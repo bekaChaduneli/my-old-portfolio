e22deab9184f15d0fcea156978659bd891c53f24
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-06-30 19:21:31 +0400

    fix navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,92 +3,74 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import styles from "./Navbar.module.scss";
 import classNames from "classnames";
+import { usePathname } from "next/navigation";
 export default function Navbar() {
-  const [activeLink, setActiveLink] = useState("main");
-  const [scrolled, setScrolled] = useState(false);
-  //   const [scrollPosition, setScrollPosition] = useState(0);
-  useEffect(() => {
-    const handleScroll = () => {
-      const position = window.pageYOffset;
-      //   setScrollPosition(position);
-      position > 110 ? setScrolled(true) : setScrolled(false);
-      console.log(position);
-    };
+    const pathname = usePathname();
+    const [scrolled, setScrolled] = useState(false);
+    //   const [scrollPosition, setScrollPosition] = useState(0);
+    useEffect(() => {
+        const handleScroll = () => {
+            const position = window.pageYOffset;
+            position > 110 ? setScrolled(true) : setScrolled(false);
+        };
 
-    window.addEventListener("scroll", handleScroll);
+        window.addEventListener("scroll", handleScroll);
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-  return (
-    <div className={styles.Navbar}>
-      <div
-        className={classNames(styles.Navbar__Box, {
-          [styles["Navbar__Box--active"]]: scrolled,
-        })}
-      >
-        <Link
-          className={styles.Navbar__Link}
-          onClick={() => {
-            setActiveLink("main");
-          }}
-          href="/"
-        >
-          <span
-            className={classNames(styles.Navbar__Text, {
-              [styles["Navbar__Text--active"]]: activeLink == "main",
-            })}
-          >
-            Main
-          </span>
-        </Link>
-        <Link
-          className={styles.Navbar__Link}
-          onClick={() => {
-            setActiveLink("about");
-          }}
-          href="/about"
-        >
-          <span
-            className={classNames(styles.Navbar__Text, {
-              [styles["Navbar__Text--active"]]: activeLink == "about",
-            })}
-          >
-            About
-          </span>
-        </Link>
-        <Link
-          onClick={() => {
-            setActiveLink("projects");
-          }}
-          className={styles.Navbar__Link}
-          href="/projects"
-        >
-          <span
-            className={classNames(styles.Navbar__Text, {
-              [styles["Navbar__Text--active"]]: activeLink == "projects",
-            })}
-          >
-            Projects
-          </span>
-        </Link>
-        <Link
-          className={styles.Navbar__Link}
-          onClick={() => {
-            setActiveLink("contact");
-          }}
-          href="/contacts"
-        >
-          <span
-            className={classNames(styles.Navbar__Text, {
-              [styles["Navbar__Text--active"]]: activeLink == "contact",
-            })}
-          >
-            Contact
-          </span>
-        </Link>
-      </div>
-    </div>
-  );
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+    useEffect(() => {
+        console.log(pathname);
+    }, [pathname]);
+    return (
+        <div className={styles.Navbar}>
+            <div
+                className={classNames(styles.Navbar__Box, {
+                    [styles["Navbar__Box--active"]]: scrolled,
+                })}
+            >
+                <Link className={styles.Navbar__Link} href="/">
+                    <span
+                        className={classNames(styles.Navbar__Text, {
+                            [styles["Navbar__Text--active"]]: pathname == "/",
+                        })}
+                    >
+                        Main
+                    </span>
+                </Link>
+                <Link className={styles.Navbar__Link} href="/about">
+                    <span
+                        className={classNames(styles.Navbar__Text, {
+                            [styles["Navbar__Text--active"]]:
+                                pathname == "/about",
+                        })}
+                    >
+                        About
+                    </span>
+                </Link>
+                <Link className={styles.Navbar__Link} href="/projects">
+                    <span
+                        className={classNames(styles.Navbar__Text, {
+                            [styles["Navbar__Text--active"]]:
+                                pathname == "/projects" ||
+                                pathname == "/projects/archive",
+                        })}
+                    >
+                        Projects
+                    </span>
+                </Link>
+                <Link className={styles.Navbar__Link} href="/contacts">
+                    <span
+                        className={classNames(styles.Navbar__Text, {
+                            [styles["Navbar__Text--active"]]:
+                                pathname == "/contacts",
+                        })}
+                    >
+                        Contact
+                    </span>
+                </Link>
+            </div>
+        </div>
+    );
 }
