76c1008c3e6a6528c648f9343d1aa0f2b425b37c
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-07-03 14:06:12 +0400

    project page html

--- src/app/projects/[id]/page.js
+++ src/app/projects/[id]/page.js
@@ -1,6 +1,9 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
+import Image from "next/image";
+import styles from "./page.module.scss";
+import AppButton from "@/components/appButton/AppButton";
 export default function Page() {
     const [data, setData] = useState(null);
     const params = useParams();
@@ -20,5 +23,61 @@ export default function Page() {
 
         fetchData();
     }, []);
-    return <div>{params.id}</div>;
+    return (
+        <div className={styles.Project}>
+            <h1 className={styles.Project__Headline}>{data?.name}</h1>
+            <div className={styles.Project__Slug}>
+                <p>{data?.slug}</p>
+            </div>
+            <figure className={styles.Project__LaptopWrapper}>
+                <img
+                    src="/images/laptop.png"
+                    alt="Laptop"
+                    className={styles.Project__Laptop}
+                />
+                {data?.videoLink ? (
+                    <video
+                        src={require(`../../../assets/videos/${data?.videoLink}.mp4`)}
+                        className={styles.Project__LaptopBackground}
+                        autoPlay
+                        loop
+                        muted
+                    />
+                ) : (
+                    <img
+                        className={styles.Project__LaptopBackgroundImage}
+                        alt="project background"
+                        src={`/images/${data?.image[0]}`}
+                    />
+                )}
+            </figure>
+            <div className={styles.Project__ButtonsWrapper}>
+                <AppButton xl href={data?.github}>
+                    Github
+                </AppButton>
+                <AppButton xl href={data?.live}>
+                    Live
+                </AppButton>
+            </div>
+            <ul className={styles.Project__TextWrapper}>
+                {data?.about.map((text, index) => {
+                    return (
+                        <li className={styles.Project__Text} key={index}>
+                            {text}
+                        </li>
+                    );
+                })}
+            </ul>
+            <figure className={styles.Project__ImagesWrapper}>
+                {data?.image.map((img) => {
+                    return (
+                        <img
+                            className={styles.Project__Images}
+                            src={`/images/${img}`}
+                        />
+                    );
+                })}
+            </figure>
+        </div>
+    );
 }
