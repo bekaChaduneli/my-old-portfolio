871f4b61915e573b08b9ead60a6a0b4ae0ecfdbb
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-07-04 03:09:18 +0400

    contact page markup

--- src/app/contacts/page.js
+++ src/app/contacts/page.js
@@ -1,11 +1,17 @@
 import Contact from "@/components/contact/Contact";
 import React from "react";
-
+import styles from "./page.module.scss";
 export default function page() {
-    return (
-        <div>
-            {" "}
-            <Contact />{" "}
-        </div>
-    );
+  return (
+    <div className={styles.Contact}>
+      <h1 className={styles.Contact__Headline}>Contact Me</h1>
+      <div className={styles.Contact__Text}>
+        <p>
+          Get in touch or shoot me an email directly on{" "}
+          <strong>beka.chaduneli.1@btu.edu.ge</strong>
+        </p>
+      </div>
+      <Contact />
+    </div>
+  );
 }
