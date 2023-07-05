fc82ab78ba7e87675cfbc374ed996e2994265959
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-07-05 18:05:35 +0400

    skills finish

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,6 +1,34 @@
-import React from "react";
-
+"use client";
+import React, { useState } from "react";
+import styles from "./skills.module.scss";
 export default function Skills({ data }) {
-    console.log(data);
-    return <div>Skills</div>;
+    const [active, setActive] = useState(null);
+    return (
+        <div className={styles.Wrapper}>
+            {data?.map((skill, index) => {
+                return (
+                    <>
+                        <span
+                            onMouseEnter={() => {
+                                setActive(skill);
+                            }}
+                            onMouseLeave={() => {
+                                setActive(null);
+                            }}
+                            key={index}
+                            className={styles.Wrapper__SkillsBox}
+                        >
+                            <img
+                                className={styles.Wrapper__SkillIcon}
+                                src={`/images${skill.icon}`}
+                            />
+                            <p className={styles.Wrapper__SkillName}>
+                                {skill.name}
+                            </p>
+                        </span>
+                    </>
+                );
+            })}
+        </div>
+    );
 }
