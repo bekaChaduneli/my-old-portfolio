d947da4784cc96ee51c193593e6d68a488d92cfd
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-07-07 18:47:30 +0400

    add projects filter

--- src/components/allProjects/AllProjects.js
+++ src/components/allProjects/AllProjects.js
@@ -5,26 +5,40 @@ import { useEffect, useRef, useState } from "react";
 import classNames from "classnames";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
 export default function AllProjects({ data }) {
     const itemsPerPage = 10;
     const videoRefs = useRef([]);
+    const [currentFramework, setCurrentFramework] = useState("");
     const [currentPage, setCurrentPage] = useState(1);
     const indexOfLastItem = currentPage * itemsPerPage;
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = data?.projects.slice(
+
+    const filteredProjects = currentFramework
+        ? data?.projects.filter((project) =>
+              project.frameworks.includes(currentFramework)
+          )
+        : data?.projects;
+
+    const currentItems = filteredProjects?.slice(
         indexOfFirstItem,
         indexOfLastItem
     );
-    const totalPages = Math.ceil(data?.projects.length / itemsPerPage);
+
+    const totalPages = Math.ceil(filteredProjects?.length / itemsPerPage);
+
     const handlePageChange = (pageNumber) => {
         setCurrentPage(pageNumber);
     };
+
     const handlePreviousPage = () => {
         setCurrentPage((prevPage) => prevPage - 1);
     };
+
     const handleNextPage = () => {
         setCurrentPage((prevPage) => prevPage + 1);
     };
+
     const handleVideoMouseEnter = (index) => {
         if (videoRefs.current[index]) {
             videoRefs.current[index].play();
@@ -66,11 +80,54 @@ export default function AllProjects({ data }) {
                 });
             });
         }
-    }, [data?.projects]);
+    }, [filteredProjects]);
+
+    const handleFrameworkClick = (framework) => {
+        setCurrentFramework(framework);
+    };
+
     return (
-        <div className={`${styles.Projects} projects`}>
-            {currentItems?.map((project, index) => {
-                return (
+        <div className={`${styles.Wrapper} projects`}>
+            <div className={styles.Projects__Filter}>
+                <span
+                    className={classNames(styles.Projects__FilterItem, {
+                        [styles["Projects__FilterItem--active"]]:
+                            currentFramework === "",
+                    })}
+                    onClick={() => handleFrameworkClick("")}
+                >
+                    All
+                </span>
+                <span
+                    className={classNames(styles.Projects__FilterItem, {
+                        [styles["Projects__FilterItem--active"]]:
+                            currentFramework === "react",
+                    })}
+                    onClick={() => handleFrameworkClick("react")}
+                >
+                    React
+                </span>
+                <span
+                    className={classNames(styles.Projects__FilterItem, {
+                        [styles["Projects__FilterItem--active"]]:
+                            currentFramework === "javascript",
+                    })}
+                    onClick={() => handleFrameworkClick("javascript")}
+                >
+                    Javascript
+                </span>
+                <span
+                    className={classNames(styles.Projects__FilterItem, {
+                        [styles["Projects__FilterItem--active"]]:
+                            currentFramework === "typescript",
+                    })}
+                    onClick={() => handleFrameworkClick("typescript")}
+                >
+                    Typescript
+                </span>
+            </div>
+            <div className={styles.Projects}>
+                {currentItems?.map((project, index) => (
                     <Link
                         key={index}
                         className={`${styles.Project__ProjectsLink} trigger`}
@@ -79,7 +136,9 @@ export default function AllProjects({ data }) {
                         href={`/projects/${project?.id}`}
                     >
                         <div
-                            style={{ height: `${project.height}px` }}
+                            style={{
+                                height: `${project.height}px`,
+                            }}
                             className={styles.Project}
                             key={index}
                         >
@@ -141,46 +200,52 @@ export default function AllProjects({ data }) {
                             </div>
                         </div>
                     </Link>
-                );
-            })}
-            <div className={styles.PaginationWrapper}>
-                <div className={styles.Pagination}>
-                    <button
-                        className={classNames(styles.Pagination__Button, {
-                            [styles["Pagination__Button--disabled"]]:
-                                currentPage === 1,
-                        })}
-                        onClick={handlePreviousPage}
-                        disabled={currentPage === 1}
-                    >
-                        {" "}
-                        <ArrowLeft className={styles.Pagination__ButtonIcon} />
-                        Previous
-                    </button>
-                    {Array.from({ length: totalPages }, (_, index) => (
-                        <span
-                            key={index}
-                            className={classNames(styles.Pagination__Element, {
-                                [styles["Pagination__Element--active"]]:
-                                    currentPage == index + 1,
+                ))}
+                <div className={styles.PaginationWrapper}>
+                    <div className={styles.Pagination}>
+                        <button
+                            className={classNames(styles.Pagination__Button, {
+                                [styles["Pagination__Button--disabled"]]:
+                                    currentPage === 1,
                             })}
-                            onClick={() => handlePageChange(index + 1)}
-                            disabled={currentPage === index + 1}
+                            onClick={handlePreviousPage}
+                            disabled={currentPage === 1}
                         >
-                            {index + 1}
-                        </span>
-                    ))}
-                    <button
-                        className={classNames(styles.Pagination__Button, {
-                            [styles["Pagination__Button--disabled"]]:
-                                currentPage === totalPages,
-                        })}
-                        onClick={handleNextPage}
-                        disabled={currentPage === totalPages}
-                    >
-                        Next
-                        <ArrowRight className={styles.Pagination__ButtonIcon} />
-                    </button>
+                            <ArrowLeft
+                                className={styles.Pagination__ButtonIcon}
+                            />
+                            Previous
+                        </button>
+                        {Array.from({ length: totalPages }, (_, index) => (
+                            <span
+                                key={index}
+                                className={classNames(
+                                    styles.Pagination__Element,
+                                    {
+                                        [styles["Pagination__Element--active"]]:
+                                            currentPage === index + 1,
+                                    }
+                                )}
+                                onClick={() => handlePageChange(index + 1)}
+                                disabled={currentPage === index + 1}
+                            >
+                                {index + 1}
+                            </span>
+                        ))}
+                        <button
+                            className={classNames(styles.Pagination__Button, {
+                                [styles["Pagination__Button--disabled"]]:
+                                    currentPage === totalPages,
+                            })}
+                            onClick={handleNextPage}
+                            disabled={currentPage === totalPages}
+                        >
+                            Next
+                            <ArrowRight
+                                className={styles.Pagination__ButtonIcon}
+                            />
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
