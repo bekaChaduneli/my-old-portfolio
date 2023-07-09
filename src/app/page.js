298e1548ad0cf0d67a0c94c8e5f0e0264cb15f14
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-07-09 23:45:52 +0400

    fix navbar

--- src/app/page.js
+++ src/app/page.js
@@ -11,91 +11,87 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 export default function Home() {
-    const [data, setData] = useState(null);
-    const scrollTriggerRef = useRef([]);
+  const [data, setData] = useState(null);
+  const scrollTriggerRef = useRef([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch("/data/bigdata.json");
-                const jsonData = await response.json();
-                setData(jsonData);
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/data/bigdata.json");
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        if (window.innerWidth >= 1024 && data) {
-            gsap.registerPlugin(ScrollTrigger);
+  useEffect(() => {
+    if (window.innerWidth >= 1024 && data) {
+      gsap.registerPlugin(ScrollTrigger);
 
-            const triggerElements = document.querySelectorAll(".trigger");
+      const triggerElements = document.querySelectorAll(".trigger");
 
-            triggerElements.forEach((triggerElement) => {
-                const tl = gsap.timeline({
-                    scrollTrigger: {
-                        trigger: triggerElement,
-                        start: "top bottom",
-                        end: "80% bottom",
-                        scrub: 1,
-                        markers: false,
-                    },
-                });
+      triggerElements.forEach((triggerElement) => {
+        const tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: triggerElement,
+            start: "top bottom",
+            end: "80% bottom",
+            scrub: 1,
+            markers: false,
+          },
+        });
 
-                tl.to(triggerElement, {
-                    y: -76,
-                    opacity: 1,
-                    duration: 1,
-                });
-                scrollTriggerRef.current.push(tl.scrollTrigger);
-            });
-        }
+        tl.to(triggerElement, {
+          y: -76,
+          opacity: 1,
+          duration: 1,
+        });
+        scrollTriggerRef.current.push(tl.scrollTrigger);
+      });
+    }
 
-        return () => {
-            scrollTriggerRef.current.forEach((trigger) => {
-                trigger.kill();
-            });
-            scrollTriggerRef.current = [];
-        };
-    }, [data]);
-    return (
-        <>
-            <div className={styles.Wrapper}>
-                <Header />
-                <div className={styles.Wrapper__Projects}>
-                    <h1 className={styles.Wrapper__Headline}>Main Projects</h1>
-                    <MainProjects data={data} />
-                    <div className={styles.Wrapper__Button}>
-                        <AppButton md type="white" href="/projects/archive">
-                            SEE MORE
-                        </AppButton>
-                    </div>
-                </div>
-                <div className={`${styles.Wrapper__Skills} trigger`}>
-                    <h1 className={styles.Wrapper__Headline}>Skills</h1>
-                    <Skills data={data?.skills} />
-                </div>
-                <div className={`${styles.Wrapper__ContactWrapper} trigger`}>
-                    <h1
-                        className={`${styles.Wrapper__Contact} contactHeadline`}
-                    >
-                        Contact Me
-                    </h1>
-                    <div
-                        className={`${styles.Wrapper__ContactText} contactText`}
-                    >
-                        <p>
-                            Get in touch or shoot me an email directly on{" "}
-                            <strong>beka.chaduneli.1@btu.edu.ge</strong>
-                        </p>
-                    </div>
+    return () => {
+      scrollTriggerRef.current.forEach((trigger) => {
+        trigger.kill();
+      });
+      scrollTriggerRef.current = [];
+    };
+  }, [data]);
+  return (
+    <>
+      <div className={styles.Wrapper}>
+        <Header />
+        <div className={styles.Wrapper__Projects}>
+          <h1 className={styles.Wrapper__Headline}>Main Projects</h1>
+          <MainProjects data={data} />
+          <div className={styles.Wrapper__Button}>
+            <AppButton md type="white" href="/projects">
+              SEE MORE
+            </AppButton>
+          </div>
+        </div>
+        <div className={`${styles.Wrapper__Skills} trigger`}>
+          <h1 className={styles.Wrapper__Headline}>Skills</h1>
+          <Skills data={data?.skills} />
+        </div>
+        <div className={`${styles.Wrapper__ContactWrapper} trigger`}>
+          <h1 className={`${styles.Wrapper__Contact} contactHeadline`}>
+            Contact Me
+          </h1>
+          <div className={`${styles.Wrapper__ContactText} contactText`}>
+            <p>
+              Get in touch or shoot me an email directly on{" "}
+              <strong>beka.chaduneli.1@btu.edu.ge</strong>
+            </p>
+          </div>
 
-                    <Contact />
-                </div>
-            </div>
-        </>
-    );
+          <Contact />
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,80 +6,74 @@ import classNames from "classnames";
 import { usePathname } from "next/navigation";
 import { gsap } from "gsap";
 export default function Navbar() {
-    const pathname = usePathname();
-    const [scrolled, setScrolled] = useState(false);
-    //   const [scrollPosition, setScrollPosition] = useState(0);
-    useEffect(() => {
-        const handleScroll = () => {
-            const position = window.pageYOffset;
-            position > 110 ? setScrolled(true) : setScrolled(false);
-        };
+  const pathname = usePathname();
+  const [scrolled, setScrolled] = useState(false);
+  //   const [scrollPosition, setScrollPosition] = useState(0);
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.pageYOffset;
+      position > 110 ? setScrolled(true) : setScrolled(false);
+    };
 
-        window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-        if (window.innerWidth >= 1024) {
-            gsap.to(".navbar", {
-                y: 60,
-                opacity: 1,
-                duration: 1,
-            });
-        }
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
-    useEffect(() => {
-        console.log(pathname);
-    }, [pathname]);
-    return (
-        <div className={`${styles.Navbar} navbar`}>
-            <div className={styles.Navbar__Box}>
-                <div
-                    className={classNames(styles.Navbar__Background, {
-                        [styles["Navbar__Background--active"]]: scrolled,
-                    })}
-                />
-                <Link className={styles.Navbar__Link} href="/">
-                    <span
-                        className={classNames(styles.Navbar__Text, {
-                            [styles["Navbar__Text--active"]]: pathname == "/",
-                        })}
-                    >
-                        Main
-                    </span>
-                </Link>
-                <Link className={styles.Navbar__Link} href="/about">
-                    <span
-                        className={classNames(styles.Navbar__Text, {
-                            [styles["Navbar__Text--active"]]:
-                                pathname == "/about",
-                        })}
-                    >
-                        About
-                    </span>
-                </Link>
-                <Link className={styles.Navbar__Link} href="/projects">
-                    <span
-                        className={classNames(styles.Navbar__Text, {
-                            [styles["Navbar__Text--active"]]:
-                                pathname == "/projects" ||
-                                pathname == "/projects/archive",
-                        })}
-                    >
-                        Projects
-                    </span>
-                </Link>
-                <Link className={styles.Navbar__Link} href="/contacts">
-                    <span
-                        className={classNames(styles.Navbar__Text, {
-                            [styles["Navbar__Text--active"]]:
-                                pathname == "/contacts",
-                        })}
-                    >
-                        Contact
-                    </span>
-                </Link>
-            </div>
-        </div>
-    );
+    if (window.innerWidth >= 1024) {
+      gsap.to(".navbar", {
+        y: 60,
+        opacity: 1,
+        duration: 1,
+      });
+    }
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <div className={`${styles.Navbar} navbar`}>
+      <div className={styles.Navbar__Box}>
+        <div
+          className={classNames(styles.Navbar__Background, {
+            [styles["Navbar__Background--active"]]: scrolled,
+          })}
+        />
+        <Link className={styles.Navbar__Link} href="/">
+          <span
+            className={classNames(styles.Navbar__Text, {
+              [styles["Navbar__Text--active"]]: pathname == "/",
+            })}
+          >
+            Main
+          </span>
+        </Link>
+        <Link className={styles.Navbar__Link} href="/about">
+          <span
+            className={classNames(styles.Navbar__Text, {
+              [styles["Navbar__Text--active"]]: pathname == "/about",
+            })}
+          >
+            About
+          </span>
+        </Link>
+        <Link className={styles.Navbar__Link} href="/projects">
+          <span
+            className={classNames(styles.Navbar__Text, {
+              [styles["Navbar__Text--active"]]:
+                pathname.slice(0, 9) == "/projects",
+            })}
+          >
+            Projects
+          </span>
+        </Link>
+        <Link className={styles.Navbar__Link} href="/contacts">
+          <span
+            className={classNames(styles.Navbar__Text, {
+              [styles["Navbar__Text--active"]]: pathname == "/contacts",
+            })}
+          >
+            Contact
+          </span>
+        </Link>
+      </div>
+    </div>
+  );
 }
