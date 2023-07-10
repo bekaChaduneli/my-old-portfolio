bff0621970b04f1cc6f19d63e8efb72e92fd6008
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-07-10 14:50:31 +0400

    fix mapping

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -28,17 +28,15 @@ export default function Skills({ data }) {
         <div className={styles.Wrapper}>
             {data?.map((skill, index) => {
                 return (
-                    <>
-                        <span key={index} className={styles.Wrapper__SkillsBox}>
-                            <img
-                                className={styles.Wrapper__SkillIcon}
-                                src={`/images${skill.icon}`}
-                            />
-                            <p className={styles.Wrapper__SkillName}>
-                                {skill.name}
-                            </p>
-                        </span>
-                    </>
+                    <span key={index} className={styles.Wrapper__SkillsBox}>
+                        <img
+                            className={styles.Wrapper__SkillIcon}
+                            src={`/images${skill.icon}`}
+                        />
+                        <p className={styles.Wrapper__SkillName}>
+                            {skill.name}
+                        </p>
+                    </span>
                 );
             })}
         </div>
